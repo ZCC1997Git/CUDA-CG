f75f357c25c28fecd26a27c7475eab30dc7d6088
zcc <zhangchenchen1997@outlook.com>
2024-01-08 10:17:41 +0800

    On branch master Changes to be committed: 	modified:   include/Vector/SpMV.cuh

--- include/Vector/SpMV.cuh
+++ include/Vector/SpMV.cuh
@@ -8,10 +8,19 @@ __global__ void SpMV(T* V, T* result, unsigned int M, unsigned int Xnode) {
     /*just use one dim thread*/
     auto tid = (blockDim.x * blockIdx.x) + threadIdx.x;
     auto step = gridDim.x * blockDim.x;
+
+    /*used warp shuffle to reduce the access of V*/
+    register T V_i, V_left, V_right;
+
     for (auto i = tid; i < M; i += step) {
-        result[i] = Addr[0][i] * V[i] + Addr[1][i] * V[i - 1 + Xnode] +
-                    Addr[2][i] * V[i + Xnode] + Addr[3][i] * V[i + 1 + Xnode] +
-                    Addr[4][i] * V[i + Xnode + Xnode];
+        V_i = V[i + Xnode];
+        V_left = __shfl_up_sync(0xFFFFFFFF, V_i, 1);
+        V_right = __shfl_down_sync(0xFFFFFFFF, V_i, 1);
+        if (threadIdx.x % 32 == 0) V_left = V[i - 1 + Xnode];
+        if (threadIdx.x % 32 == 31) V_right = V[i + 1 + Xnode];
+
+        result[i] = Addr[0][i] * V[i] + Addr[1][i] * V_left + Addr[2][i] * V_i +
+                    Addr[3][i] * V_right + Addr[4][i] * V[i + Xnode + Xnode];
     }
 }
 #endif
